e938f68b4eec756e42ed1e9ea3abe845a6792fb1
Sarankumarbe <sarankumar301201@gmail.com>
2025-07-23 19:48:02 +0530

    token stored cookies

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 exports.protect = (req, res, next) => {
-  let token = req.headers.authorization;
+  // let token = req.headers.authorization;
 
-  if (!token || !token.startsWith('Bearer ')) {
+  // if (!token || !token.startsWith('Bearer ')) {
+  //   return res.status(401).json({ message: 'Not authorized, token missing' });
+  // }
+
+  // token = token.split(' ')[1];
+
+  const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
+
+  if (!token) {
     return res.status(401).json({ message: 'Not authorized, token missing' });
   }
-
-  token = token.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoute');
 
 const app = express();
 
+// app.use(cors({
+//   origin: process.env.CLIENT_URL,
+//   credentials: true
+// }));
+
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // Log every request
 app.use((req, res, next) => {
