d0f04f412a82f495b8906d6d14dbbc3b88a2e5f0
Parvez0610 <parvezmusharaf0619@gmail.com>
2025-07-27 00:35:48 +0530

    question paper get, updates, delete apis

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -5,8 +5,15 @@ const { signup, login } = require("../controllers/authController");
 const {
   uploadAndParseQuestions,
   saveQuestions,
+  getAllQuestionPapers,
+  getQuestionPaper,
+  updateQuestionPaper,
+  deleteQuestionPaper,
 } = require("../controllers/questionPaperController");
 const { handleUpload } = require("../middleware/uploadMiddleware");
+
+adminrouter.get("/question-papers", getAllQuestionPapers);
+adminrouter.get("/question-paper/:id", getQuestionPaper);
 
 adminrouter.post("/login", login);
 adminrouter.post(
@@ -15,5 +22,9 @@ adminrouter.post(
   uploadAndParseQuestions
 );
 adminrouter.post("/save-questions", saveQuestions);
+
+adminrouter.put("/update-question-paper/:id", updateQuestionPaper);
+
+adminrouter.put("/delete-question-paper/:id", deleteQuestionPaper);
 
 module.exports = adminrouter;
